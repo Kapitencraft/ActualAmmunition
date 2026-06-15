161d7dc8b7ceb12d388c72bb603ea427468b7b6f
Kapitencraft <kapitencraft@gmail.com>
2026-06-15 16:48:59 +0200

    added items and wired them into the projectile types

--- src/main/java/com/example/examplemod/datagen/ModCreatePotatoProjectileTypes.java
+++ src/main/java/com/example/examplemod/datagen/ModCreatePotatoProjectileTypes.java
@@ -1,6 +1,7 @@
 package com.example.examplemod.datagen;
 
 import com.example.examplemod.CreateActualAmmunition;
+import com.example.examplemod.registry.ModItems;
 import com.simibubi.create.api.equipment.potatoCannon.PotatoCannonProjectileType;
 import com.simibubi.create.api.registry.CreateRegistries;
 import net.minecraft.data.worldgen.BootstrapContext;
@@ -11,16 +12,28 @@ public class ModCreatePotatoProjectileTypes {
     private static final ResourceKey<PotatoCannonProjectileType> EXPLOSIVE_POMEGRANATE = key("explosive_pomegranate");
     private static final ResourceKey<PotatoCannonProjectileType> COCONUT = key("coconut");
     private static final ResourceKey<PotatoCannonProjectileType> HEAVY_COCONUT = key("heavy_coconut");
+    private static final ResourceKey<PotatoCannonProjectileType> CORN = key("corn");
 
     private static ResourceKey<PotatoCannonProjectileType> key(String name) {
         return ResourceKey.create(CreateRegistries.POTATO_PROJECTILE_TYPE, CreateActualAmmunition.res(name));
     }
 
     public static void bootstrap(BootstrapContext<PotatoCannonProjectileType> context) {
-        context.register(POMEGRANATE, new PotatoCannonProjectileType.Builder().build());
-        context.register(EXPLOSIVE_POMEGRANATE, new PotatoCannonProjectileType.Builder().build());
-        context.register(COCONUT, new PotatoCannonProjectileType.Builder().build());
-        context.register(HEAVY_COCONUT, new PotatoCannonProjectileType.Builder().build());
+        context.register(POMEGRANATE, new PotatoCannonProjectileType.Builder()
+                        .addItems(ModItems.POMEGRANATE)
+                .build());
+        context.register(EXPLOSIVE_POMEGRANATE, new PotatoCannonProjectileType.Builder()
+                        .addItems(ModItems.EXPLOSIVE_POMEGRANATE)
+                .build());
+        context.register(COCONUT, new PotatoCannonProjectileType.Builder()
+                        .addItems(ModItems.COCONUT)
+                .build());
+        context.register(HEAVY_COCONUT, new PotatoCannonProjectileType.Builder()
+                        .addItems(ModItems.HEAVY_COCONUT)
+                .build());
+        context.register(CORN, new PotatoCannonProjectileType.Builder()
+                .addItems(ModItems.CORN)
+                .build());
 
     }
 }
--- src/main/java/com/example/examplemod/registry/ModItems.java
+++ src/main/java/com/example/examplemod/registry/ModItems.java
@@ -5,8 +5,14 @@ import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-public class ModItems {
+public interface ModItems {
     DeferredRegister.Items REGISTRY = DeferredRegister.createItems(CreateActualAmmunition.MODID);
 
     DeferredItem<Item> POMEGRANATE = REGISTRY.registerItem("pomegranate", Item::new);
+    DeferredItem<Item> EXPLOSIVE_POMEGRANATE = REGISTRY.registerItem("explosive_pomegranate", Item::new);
+
+    DeferredItem<Item> COCONUT = REGISTRY.registerItem("coconut", Item::new);
+    DeferredItem<Item> HEAVY_COCONUT = REGISTRY.registerItem("heavy_coconut", Item::new);
+
+    DeferredItem<Item> CORN = REGISTRY.registerItem("corn", Item::new);
 }
